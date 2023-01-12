4cd5f0415e14af45ab4fcd19404d755fd0574f5e
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-12 17:47:25 +0100

    [enhancement]create exemple component and display btn

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+// ASSETS
+import close from './assets/close.png';
+import check from './assets/check.png';
+
+// COMPONENT
+import useModal from './component/useModal';
+
+// EXPORTS
+export { close };
+export { check };
+export { useModal };
